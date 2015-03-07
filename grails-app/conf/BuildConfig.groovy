b21be82f9d5de77daa50104a3f24c4d288813cf6
lucasr20 <lucasrossi20@gmail.com>
2015-03-07 17:42:34 -0300

    agregando plugin mail y datos mail

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -54,10 +54,13 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
+
+        
         // plugins for the build system only
         build ":tomcat:7.0.55"
 
         // plugins for the compile step
+        compile ":mail:1.0.7"
         compile ":scaffolding:2.1.2"
         compile ':cache:1.1.8'
         compile ":asset-pipeline:1.9.9"
