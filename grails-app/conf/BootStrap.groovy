67d580c0b9d6753a0eab34e46aae122740ee7bab
lucasr20 <lucasrossi20@gmail.com>
2015-03-10 09:46:15 -0300

    empleados creados para el dia actual

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -10,9 +10,9 @@ class BootStrap {
     //Empleados Prueba
     Empleado e1 = new Empleado(nombre: "Alberto", apellido: "Ramirez", fechaNacimiento: Date.parse("yyyy/MM/dd", "1975/7/06"));
     Empleado e2 = new Empleado(nombre: "Carlos", apellido: "Rossi", fechaNacimiento: Date.parse("yyyy/MM/dd", "1965/10/12"));
-    Empleado e3 = new Empleado(nombre: "Rodolfo", apellido: "Pert", fechaNacimiento: Date.parse("yyyy/MM/dd", "1980/03/07"));
-    Empleado e4 = new Empleado(nombre: "Juan", apellido: "Perez", fechaNacimiento: Date.parse("yyyy/MM/dd", "1955/09/20"));
-    Empleado e5 = new Empleado(nombre: "Mario", apellido: "Lerva", fechaNacimiento: Date.parse("yyyy/MM/dd", "1972/01/13"));
+    Empleado e3 = new Empleado(nombre: "Rodolfo", apellido: "Pert", fechaNacimiento: new Date());
+    Empleado e4 = new Empleado(nombre: "Juan", apellido: "Perez", fechaNacimiento: new Date());
+    Empleado e5 = new Empleado(nombre: "Mario", apellido: "Lerva", fechaNacimiento: new Date());
 	
     e1.save();
     e2.save();
