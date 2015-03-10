4314399f9eb088c31dca6a03ef999a2d5c025d42
lucasr20 <lucasrossi20@gmail.com>
2015-03-10 11:40:19 -0300

    url mapping /

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -8,17 +8,17 @@ class BootStrap {
     
 
     //Empleados Prueba
-    Empleado e1 = new Empleado(nombre: "Alberto", apellido: "Ramirez", fechaNacimiento: Date.parse("yyyy/MM/dd", "1975/7/06"));
-    Empleado e2 = new Empleado(nombre: "Carlos", apellido: "Rossi", fechaNacimiento: Date.parse("yyyy/MM/dd", "1965/10/12"));
-    Empleado e3 = new Empleado(nombre: "Rodolfo", apellido: "Pert", fechaNacimiento: new Date());
-    Empleado e4 = new Empleado(nombre: "Juan", apellido: "Perez", fechaNacimiento: new Date());
-    Empleado e5 = new Empleado(nombre: "Mario", apellido: "Lerva", fechaNacimiento: new Date());
-	
-    e1.save();
-    e2.save();
-    e3.save();
-    e4.save();
-    e5.save();
+    //Empleado e1 = new Empleado(nombre: "Alberto", apellido: "Ramirez", fechaNacimiento: Date.parse("yyyy/MM/dd", "1975/7/06"));
+    //Empleado e2 = new Empleado(nombre: "Carlos", apellido: "Rossi", fechaNacimiento: Date.parse("yyyy/MM/dd", "1965/10/12"));
+    //Empleado e3 = new Empleado(nombre: "Rodolfo", apellido: "Pert", fechaNacimiento: new Date());
+    //Empleado e4 = new Empleado(nombre: "Juan", apellido: "Perez", fechaNacimiento: new Date());
+    //Empleado e5 = new Empleado(nombre: "Mario", apellido: "Lerva", fechaNacimiento: new Date());
+	//
+    //e1.save();
+    //e2.save();
+    //e3.save();
+    //e4.save();
+    //e5.save();
 
 
     }
--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -7,7 +7,7 @@ class UrlMappings {
             }
         }
 
-        "/"(view:"/index")
+        "/"(view:"/empleados/index")
         "500"(view:'/error')
 	}
 }
