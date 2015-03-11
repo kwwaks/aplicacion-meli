9e8d975cfe790c1bc69d3e764071215c9f0d8d8a
lucasr20 <lucasrossi20@gmail.com>
2015-03-11 11:32:49 -0300

    abm empleado rest, por ahora solo crear empleado

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -87,6 +87,8 @@ grails.web.disable.multipart=false
 // request parameters to mask when logging exceptions
 grails.exceptionresolver.params.exclude = ['password']
 
+grails.databinding.dateFormats = ['yyyy/MM/dd']
+
 // configure auto-caching of queries by default (if false you can cache individual queries with 'cache: true')
 grails.hibernate.cache.queries = false
 
