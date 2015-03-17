eb7059a19da4a35754d2b4ed122b35a3b3ec87c6
kwwaks <ken.weinberg@mercadolibre.com>
2015-03-17 11:01:17 -0300

    Funcionando el asignar regalos

--- grails-app/services/regalos/RegaloService.groovy
+++ grails-app/services/regalos/RegaloService.groovy
@@ -8,14 +8,14 @@ import grails.transaction.Transactional
 @Transactional
 class RegaloService {
 
-	def agregar(miEmpleado, miRegalo){
+	def agregar(Empleado miEmpleado, Regalo miRegalo){
 		def presente = miEmpleado.regalos.find{it.fechaProducto.year == new Date().year};
 		if(presente != null){
 			miEmpleado.regalos.remove(presente);
 			presente.delete(flush:true);
 		}
 		miRegalo.save(flush:true);
-		miEmpleado.regalos.add(miRegalo);
+		miEmpleado.addToRegalos(miRegalo);
 		miEmpleado.save(flush:true);
 	}
 
